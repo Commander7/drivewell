50aa34fa810602b141ced83ec4d2a91e247487e2
Commander7 <Marcellusvii@hotmail.com>
2024-03-25 16:34:40 +0100

    pages

---
+++ About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        In early May, trade publication Drinks International (DI) released its
+        list of the past year’s most popular cocktails around the globe. To
+        create the annual ranking, DI surveys the world’s top bars and asks each
+        to name its best-selling drinks for the year. The responses are then
+        weighted and ranked. This year, the publication surveyed 100 bars from
+        33 countries to create a roundup of the year’s most in-demand libations.
+        The shoo-in classics like Martinis, Manhattans, and Old Fashioneds are
+        all still present, but this year’s list saw a staggering number of
+        cocktail debuts, particularly those in the gin and mezcal categories.
+        Stagnation is boring, so it’s always nice to see some new talent.
+      </p>
+    </section>
+  );
+};
+export default About;
